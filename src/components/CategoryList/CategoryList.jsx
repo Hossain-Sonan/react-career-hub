e8b813553cf63bfec6fc77165f9573debd771243
Alaul1988 Sunan <alaul1988sunan@gmail.com>
2023-12-12 16:37:31 -0500

    show-all-button-add

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,13 @@
+
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h2 className="text-5xl text-center">Job Category List</h2>
+            <p className="text-center">I'm sorry, but your request is a bit unclear.
+             </p>
+        </div>
+    );
+};
+
+export default CategoryList;
